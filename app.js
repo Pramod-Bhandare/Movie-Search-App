aba13bc22525d2b07753f4856a76298d3f6f6bbe
Pramod Bhandare <pramodbhandare94@gmail.com>
2025-09-27 22:23:47 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-const API_KEY = {"Title":"Guardians of the Galaxy Vol. 2","Year":"2017","Rated":"PG-13","Released":"05 May 2017","Runtime":"136 min","Genre":"Action, Adventure, Comedy","Director":"James Gunn","Writer":"James Gunn, Dan Abnett, Andy Lanning","Actors":"Chris Pratt, Zoe Saldaña, Dave Bautista","Plot":"The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father, the ambitious celestial being Ego.","Language":"English","Country":"United States","Awards":"Nominated for 1 Oscar. 15 wins & 60 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.6/10"},{"Source":"Rotten Tomatoes","Value":"85%"},{"Source":"Metacritic","Value":"67/100"}],"Metascore":"67","imdbRating":"7.6","imdbVotes":"806,353","imdbID":"tt3896198","Type":"movie","DVD":"N/A","BoxOffice":"$389,813,101","Production":"N/A","Website":"N/A","Response":"True"};
+// 👇 Replace with your actual API key from TMDB dashboard
+const API_KEY = "6c2a6dcb1f23a4567e9d12345abcdef6";  
 
 const APIURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 const IMGPATH = "https://image.tmdb.org/t/p/w500";
@@ -9,9 +9,13 @@ const movieBox = document.querySelector("#movie-box");
 
 // Fetch movies
 const getMovies = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  showMovies(data.results);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    showMovies(data.results);
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+  }
 };
 
 // Display movies
